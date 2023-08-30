d1e1b0adf6120765f2859656975db0f280cb8d9c
Shubham Chauhan <64500580+shubhamppl@users.noreply.github.com>
2023-08-30 17:39:12 +0530

    Update assisgnment_2_musigma.sql

--- assisgnment_2_musigma.sql
+++ assisgnment_2_musigma.sql
@@ -2,15 +2,15 @@
 for stores that have incurred losses flag them as "DRAINS", for stores that have amassed profits in the range $2000-$9999, 
 flag them as "AVERAGE PROFIT STORES" and the rest should be flagged as "NON PROFIT STORES" 
 Profit = [(SalesQuantity*UnitPrice)-(DiscountQuantity*DiscountAmount)] - (SalesQuantity*UnitCost) */
-select storekey ,
-case when profit > 10000 then 'yes yes yes '
-                    when profit >1000 and profit < 4000 then 'ok ok ok'
+
+select storekey ,profit, case
+                    when profit > 10000 then 'yes yes yes '
+                    when profit >1000 and profit < 9999 then 'ok ok ok'
 					when profit < 1000 then 'nooooooo' 
-					else 'nothig '
 					end as yo
 from  (
 select StoreKey,
-count(SalesQuantity*UnitPrice-(DiscountAmount*DiscountQuantity)-(SalesQuantity*UnitCost)) as profit 
-from [ContosoRetailDW].[dbo].[FactSales]
-group by StoreKey
-)as profittt
+count(SalesQuantity*UnitPrice-(DiscountAmount*DiscountQuantity)-(SalesQuantity*UnitCost)) 
+as profit 
+from [ContosoRetailDW].[dbo].[FactSales] 
+group by StoreKey)as profit_2;
