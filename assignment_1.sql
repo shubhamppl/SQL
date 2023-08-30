18e377d4fa486823139d502b952cb8fde34eb0b1
Shubham Chauhan <64500580+shubhamppl@users.noreply.github.com>
2023-08-30 16:27:33 +0530

    Create assignment_1.sql
    ass 1 of sql

---
+++ assignment_1.sql
@@ -0,0 +1,10 @@
+/*
+6	Write a query to return count of male customer based on occupation	dbo.DimCustomer
+7	write a query to get records whose income > 50000	dbo.DimCustomer
+8	write a query to return records who are Female House Owners 	dbo.DimCustomer
+9	write a query to return CustomerKey whose education is  bachelors with number of children >0	dbo.DimCustomer
+10	Theory: Group By, Order of Execution	*/
+
+select  Gender,Occupation, count(*) as new from [ContosoRetailDW].[dbo].[DimCustomer]
+where gender ='m'
+group  by Occupation,Gender
